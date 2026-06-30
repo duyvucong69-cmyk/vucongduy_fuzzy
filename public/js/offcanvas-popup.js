4bf865f5b1f8aa8bbe6d3c45e8f59dc11bb9421a
Fuzzy Test Developer <test@example.com>
2026-06-30 11:25:49 +0700

    Fix offcanvas null check

--- public/js/offcanvas-popup.js
+++ public/js/offcanvas-popup.js
@@ -3,6 +3,8 @@ offcanvas js
  ==========================*/
 window.addEventListener("load", (event) => {
   var myOffcanvas = document.getElementById("offcanvas");
-  var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
-  bsOffcanvas.show();
+  if (myOffcanvas) {
+    var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
+    bsOffcanvas.show();
+  }
 });
